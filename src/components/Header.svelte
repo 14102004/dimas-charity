303b928df293a9bcbab06fba23d770f86df9e007
Dimas Wardoyo <kong87013@gmail.com>
2022-02-15 22:42:39 +0700

    aku

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -30,7 +30,7 @@
               <span class="badge">
                 <i class="fa fa-heart" />
               </span>
-              Donasi Sekarang
+              Kontol
             </a>
           </div>
           <!-- .xs-navs-button END -->
