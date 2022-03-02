2453028e242cbd30bb8158e7a8275aa5a409ec62
Dimas Wardoyo <kong87013@gmail.com>
2022-03-02 17:42:45 +0700

    facebook

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,114 +1,117 @@
 <!-- footer section start -->
 <footer class="xs-footer-section">
-    <div class="container">
-      <div class="xs-footer-top-layer">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 footer-widget xs-pr-20">
-            <a href="/" class="xs-footer-logo">
-              <img src="/assets/images/footer_logo.png" alt="" />
-            </a>
-            <p>
-              CharityPress Online dan mengumpulkan uang untuk amal dan menyebabkan Anda
-              tertarik tentang. CharityPress adalah inovatif, hemat biaya
-              on line.
-            </p>
-            <ul class="xs-social-list-v2">
-              <li>
-                <a href="" class="color-facebook">
-                  <i class="fa fa-facebook" />
-                </a>
-              </li>
-              <li>
-                <a href="" class="color-twitter">
-                  <i class="fa fa-twitter" />
-                </a>
-              </li>
-              <li>
-                <a href="" class="color-dribbble">
-                  <i class="fa fa-dribbble" />
-                </a>
-              </li>
-              <li>
-                <a href="" class="color-pinterest">
-                  <i class="fa fa-pinterest" />
-                </a>
-              </li>
-            </ul>
-            <!-- .xs-social-list END -->
-          </div>
-          <div class="col-lg-4 col-md-6 footer-widget">
-            <h3 class="widget-title">About Us</h3>
-            <ul class="xs-footer-list">
-              <li>
-                <a href="/">Tentang karyawan</a>
-              </li>
-              <li>
-                <a href="#">Bagaimana itu bekerja</a>
-              </li>
-              <li>
-                <a href="#">Karir</a>
-              </li>
-              <li>
-                <a href="#">tekan</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-4 col-md-6 footer-widget">
-            <h3 class="widget-title">Hubungi kami</h3>
-            <ul class="xs-info-list">
-              <li>
-                <i class="fa fa-home" />
-                Sector # 48, 123 Street, miosya road VIC 28, Australia.
-              </li>
-              <li>
-                <i class="fa fa-phone" />
-                (800) 123.456.7890 (800) 123.456.7890 +00 99 88 5647
-              </li>
-              <li>
-                <i class="fa fa-envelope-o" />
-                <a href="mailto:kong87013@gmail.com">kong87013@gmail.com</a>
-              </li>
-            </ul>
-            <!-- .xs-list-with-icon END -->
-          </div>
+  <div class="container">
+    <div class="xs-footer-top-layer">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-widget xs-pr-20">
+          <a href="/" class="xs-footer-logo">
+            <img src="/assets/images/footer_logo.png" alt="" />
+          </a>
+          <p>
+            CharityPress Online dan mengumpulkan uang untuk amal dan menyebabkan
+            Anda tertarik tentang. CharityPress adalah inovatif, hemat biaya on
+            line.
+          </p>
+          <ul class="xs-social-list-v2">
+            <li>
+              <a
+                href="https://www.facebook.com/dimas.wardoyo.9465"
+                class="color-facebook"
+              >
+                <i class="fa fa-facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="" class="color-twitter">
+                <i class="fa fa-twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="" class="color-dribbble">
+                <i class="fa fa-dribbble" />
+              </a>
+            </li>
+            <li>
+              <a href="" class="color-pinterest">
+                <i class="fa fa-pinterest" />
+              </a>
+            </li>
+          </ul>
+          <!-- .xs-social-list END -->
+        </div>
+        <div class="col-lg-4 col-md-6 footer-widget">
+          <h3 class="widget-title">About Us</h3>
+          <ul class="xs-footer-list">
+            <li>
+              <a href="/">Tentang karyawan</a>
+            </li>
+            <li>
+              <a href="#">Bagaimana itu bekerja</a>
+            </li>
+            <li>
+              <a href="#">Karir</a>
+            </li>
+            <li>
+              <a href="#">tekan</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-4 col-md-6 footer-widget">
+          <h3 class="widget-title">Hubungi kami</h3>
+          <ul class="xs-info-list">
+            <li>
+              <i class="fa fa-home" />
+              Sector # 48, 123 Street, miosya road VIC 28, Australia.
+            </li>
+            <li>
+              <i class="fa fa-phone" />
+              (800) 123.456.7890 (800) 123.456.7890 +00 99 88 5647
+            </li>
+            <li>
+              <i class="fa fa-envelope-o" />
+              <a href="mailto:kong87013@gmail.com">kong87013@gmail.com</a>
+            </li>
+          </ul>
+          <!-- .xs-list-with-icon END -->
         </div>
       </div>
     </div>
-    <div class="container">
-      <div class="xs-copyright">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="xs-copyright-text">
-              <p>&copy; Copyright 2022 Charity. - All Right's Reserved</p>
-            </div>
+  </div>
+  <div class="container">
+    <div class="xs-copyright">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="xs-copyright-text">
+            <p>&copy; Copyright 2022 Charity. - All Right's Reserved</p>
           </div>
-          <div class="col-md-6">
-            <nav class="xs-footer-menu">
-              <ul>
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-                <li>
-                  <a href="#">Help Desk</a>
-                </li>
-                <li>
-                  <a href="#">Support</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+        </div>
+        <div class="col-md-6">
+          <nav class="xs-footer-menu">
+            <ul>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Help Desk</a>
+              </li>
+              <li>
+                <a href="#">Support</a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
-    <div class="xs-back-to-top-wraper">
-      <a href="#" class="xs-back-to-top">
-        <i class="fa fa-angle-up" />
-      </a>
-    </div>
-  </footer>
+  </div>
+  <div class="xs-back-to-top-wraper">
+    <a href="#" class="xs-back-to-top">
+      <i class="fa fa-angle-up" />
+    </a>
+  </div>
+</footer>
